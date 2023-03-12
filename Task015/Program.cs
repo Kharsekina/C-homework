5c40b082d85fa75b705b1206fd905553375ea757
Oksana Kharsekina <oksanaverbilova@gmail.com>
2023-03-12 15:54:07 +0700

    Решение з0адачи 15

---
+++ Task015/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+Console.WriteLine("Ведите цифру");
+int number=Convert.ToInt32(Console.ReadLine());
+
+if (number<1 || number>7) Console.WriteLine("Некорректное число, введите число от 1 до 7");
+else WeekDays(number);
+
+
+bool WeekDays(int num)
+{
+    if(num>5) 
+    {
+        Console.WriteLine("да");
+        return true;
+    }
+    Console.WriteLine("нет");
+    return false;
+}
