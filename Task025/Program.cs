b1a5fe0810b7867e35d1de92bae1d02608d29c38
Oksana Kharsekina <oksanaverbilova@gmail.com>
2023-03-14 22:31:48 +0700

    Решение задачи 25

---
+++ Task025/Program.cs
@@ -0,0 +1,35 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+Console.WriteLine("Введите первое число");
+int numberA=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int numberB=Convert.ToInt32(Console.ReadLine());
+
+if (Natural(numberB))
+{
+    Console.WriteLine($"{numberA}, {numberB} ->{Pow(numberA, numberB)}");
+}
+
+bool Natural(int num)
+{
+    if (num<0) 
+    {
+        Console.WriteLine("Введите натуральное число");
+        return false;
+    }  
+    return true;    
+}   
+
+int Pow(int num1, int num2)
+{
+    if (num2 == 0) num1 = 1;
+
+    int product=1;
+    for(int i=0; i< num2; i++)
+    {
+         product= product*num1;
+    }
+    return product;
+}
