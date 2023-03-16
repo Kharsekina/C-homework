405ed00bf44125434cd936d363353d995bb35377
Oksana Kharsekina <oksanaverbilova@gmail.com>
2023-03-16 14:53:35 +0700

    Решение задачи 38

---
+++ Task038/Program.cs
@@ -0,0 +1,48 @@
+﻿// Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+// [3.5, 7.1, 22.9, 2.3, 78.5] -> 76.2
+
+double[] FillArray(int size, int min, int max)
+{
+    double[] arr= new double[size];
+    Random rnd= new Random();
+    for (int i = 0; i < size; i++)
+    {
+        double num=rnd.NextDouble()*(max-min)+min;
+        arr[i]=Math.Round(num, 1);
+    }
+    return arr;
+}
+
+void PrintArray(double []arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]}  ");
+    }
+}
+
+double MinDigit(double[]arr)
+{
+    double minPosition=arr[0];
+    for (int i = 1; i < arr.Length; i++)
+    {
+        if(arr[i]<minPosition) minPosition=arr[i];
+    }
+    return minPosition;
+}
+
+double MaxDigit(double[]arr)
+{
+    double maxPosition=arr[0];
+    for (int i = 1; i < arr.Length; i++)
+    {
+        if(arr[i]>maxPosition) maxPosition=arr[i];
+    }
+    return maxPosition;
+}
+
+double [] array= FillArray(5,0,10);
+PrintArray(array);
+Console.WriteLine();
+double difference= Math.Round(MaxDigit(array)-MinDigit(array), 1);
+Console.WriteLine(difference);
